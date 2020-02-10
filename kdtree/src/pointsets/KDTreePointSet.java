121a386643965f43d2a2075514ace3cec5d169ed
Ryan Lee <ralee478.school@gmail.com>
2020-02-09 17:10:00 -0800

    I think this works

--- kdtree/src/pointsets/KDTreePointSet.java
+++ kdtree/src/pointsets/KDTreePointSet.java
@@ -7,7 +7,21 @@ import java.util.List;
  * Fast nearest-neighbor implementation using a k-d tree.
  */
 public class KDTreePointSet<T extends Point> implements PointSet<T> {
-    // TODO: add fields as necessary
+
+    private class KDNode {
+        KDNode less;
+        KDNode more;
+        T data;
+
+        public KDNode(T data) {
+            this.less = null;
+            this.more = null;
+            this.data = data;
+        }
+    }
+
+    private KDNode root;
+    private List<T> points;
 
     /**
      * Instantiates a new KDTreePointSet with a shuffled version of the given points.
@@ -30,25 +44,110 @@ public class KDTreePointSet<T extends Point> implements PointSet<T> {
      * @param points a non-null, non-empty list of points to include.
      *               Assumes that the list will not be used externally afterwards (and thus may
      *               directly store and mutate the array).
+     *
+     * Learned about handling up/down vs left/right insertions from:
+     * https://github.com/mgruben/Kd-Trees/blob/master/KdTree.java
      */
     KDTreePointSet(List<T> points) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        for (T point : points) {
+            this.root = buildTree(this.root, point, true);
+        }
+        this.points = points;
+    }
+
+    private KDNode buildTree(KDNode node, T point, boolean leftRight) {
+        // When you reach the end of the tree
+        if (node == null) {
+            return new KDNode(point);
+        }
+        double compare = compareLocation(node, point, leftRight);
+
+        // Whichever value is lesser is put on the lesser side
+        // Compare determines whether to compare at x or y
+        if (compare < 0) {
+            node.less = buildTree(node.less, point, !leftRight);
+
+            // Whichever value is greater is put on the greater side
+            // Compare determines whether to compare at x or y
+        } else if (compare > 0) {
+            node.more = buildTree(node.more, point, !leftRight);
+
+            // Nodes on same dividing line that are not the same
+            // are automatically put left
+        } else if (!node.data.equals(point)) {
+            node.less = buildTree(node.less, point, !leftRight);
+        }
+        return node;
+    }
+
+    // if comparing left and right compare x
+    // if comparing up and down compare y
+    private double compareLocation(KDNode node, T point, boolean leftRight) {
+        if (leftRight) {
+            return point.x() - node.data.x();
+        } else {
+            return point.y() - node.data.y();
+        }
     }
 
     /**
      * Returns the point in this set closest to the given point in (usually) O(log N) time, where
      * N is the number of points in this set.
+     * <p>
+     * Pruning conditions inspired by https://github.com/mgruben/Kd-Trees/blob/master/KdTree.java
      */
     @Override
     public T nearest(Point target) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return nearest(root, target, root.data, true);
+    }
+
+    private T nearest(KDNode current, Point target, T closest, boolean leftRight) {
+
+        // return last point in tree
+        if (current == null) {
+            return closest;
+        }
+
+        // return a direct click on point
+        if (current.data.equals(target)) {
+            return (T) target;
+        }
+
+        // if the current point is closer to the target than the closest pt from before,
+        // current pt is now closest pt
+        if (current.data.distanceSquaredTo(target) <
+            closest.distanceSquaredTo(target)) {
+            closest = current.data;
+        }
+
+        // Determines which side to most likely look at
+        double section = compareLocation(current, (T) target, leftRight);
+
+        // If pt is expected to be on the left/bottom side
+        if (section <= 0) {
+            closest = nearest(current.less, target, closest, !leftRight);
+
+            // if the distance from the current point is less than the distance from the closest point,
+            // we explore the other side
+            if (Math.pow(section, 2) < closest.distanceSquaredTo(target)) {
+                closest = nearest(current.more, target, closest, !leftRight);
+            }
+
+            // pt expected to be on right/top side
+        } else {
+            closest = nearest(current.more, target, closest, !leftRight);
+
+            // if the distance from the current point is less than the distance from the closest point,
+            // we explore the other side
+            if (Math.pow(section, 2) < closest.distanceSquaredTo(target)) {
+                closest = nearest(current.less, target, closest, !leftRight);
+            }
+        }
+        return closest;
     }
 
     @Override
     public List<T> allPoints() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return this.points;
     }
 }
--- kdtree/src/pointsets/NaivePointSet.java
+++ kdtree/src/pointsets/NaivePointSet.java
@@ -6,17 +6,18 @@ import java.util.List;
  * Naive nearest-neighbor implementation using a linear scan.
  */
 public class NaivePointSet<T extends Point> implements PointSet<T> {
-    // TODO: add fields as necessary
+
+    private List<T> pointList;
 
     /**
      * Instantiates a new NaivePointSet with the given points.
+     *
      * @param points a non-null, non-empty list of points to include
      *               Assumes that the list will not be used externally afterwards (and thus may
      *               directly store and mutate the array).
      */
     public NaivePointSet(List<T> points) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.pointList = points;
     }
 
     /**
@@ -25,13 +26,20 @@ public class NaivePointSet<T extends Point> implements PointSet<T> {
      */
     @Override
     public T nearest(Point target) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        double initial = pointList.get(0).distanceSquaredTo(target);
+        T nearest = pointList.remove(0);
+        for (T point : this.pointList) {
+            double compare = point.distanceSquaredTo(target);
+            if (compare < initial) {
+                nearest = point;
+                initial = compare;
+            }
+        }
+        return nearest;
     }
 
     @Override
     public List<T> allPoints() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return pointList;
     }
 }
