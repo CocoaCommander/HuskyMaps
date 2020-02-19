06c55f92178adcbc11128f0b66c478aca298279b
Ryan Lee <ralee478.school@gmail.com>
2020-02-18 23:56:21 -0800

    I think this works

--- astar/src/graphpathfinding/AStarPathFinder.java
+++ astar/src/graphpathfinding/AStarPathFinder.java
@@ -1,30 +1,93 @@
 package graphpathfinding;
 
+import priorityqueues.DoubleMapMinPQ;
+import timing.Timer;
+
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @see ShortestPathFinder for more method documentation
  */
 public class AStarPathFinder<VERTEX> extends ShortestPathFinder<VERTEX> {
-    // TODO: add fields as necessary
 
+    private AStarGraph<VERTEX> graph;
     /**
      * Creates a new AStarPathFinder that works on the provided graph.
      */
     public AStarPathFinder(AStarGraph<VERTEX> graph) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.graph = graph;
     }
 
     @Override
     public ShortestPathResult<VERTEX> findShortestPath(VERTEX start, VERTEX end, Duration timeout) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+        Timer timer = new Timer(timeout);
+        DoubleMapMinPQ<VERTEX> fringe = new DoubleMapMinPQ<>();
+        List<VERTEX> solution = new ArrayList<>();
+        Map<VERTEX, Double> distances = new HashMap<>();
+        Map<VERTEX, VERTEX> prevPoint = new HashMap<>();
+        int states = 0;
+
+        fringe.add(start, this.graph.estimatedDistanceToGoal(start, end));
+        distances.put(start, 0.0);
+
+        while (!fringe.isEmpty() && !timer.isTimeUp()) {
+            VERTEX min = fringe.removeMin();
+            if (min.equals(end)) {
+                break;
+            }
+            states++;
+            for (WeightedEdge<VERTEX> neighbor : this.graph.neighbors(min)) {
+                if (distances.containsKey(neighbor.to()) &&
+                    distances.get(neighbor.to()) >
+                        neighbor.weight() + distances.get(min)) {
+
+                    distances.put(neighbor.to(), neighbor.weight() +
+                        distances.get(min));
+                    prevPoint.put(neighbor.to(), min);
+                    fringe.changePriority(neighbor.to(), neighbor.weight() +
+                        distances.get(min) +
+                        this.graph.estimatedDistanceToGoal(min, end));
+                } else if (!distances.containsKey(neighbor.to())) {
+                    distances.put(neighbor.to(), neighbor.weight() +
+                        distances.get(min));
+                    prevPoint.put(neighbor.to(), min);
+                    fringe.add(neighbor.to(),
+                        neighbor.weight() + distances.get(min) +
+                            this.graph.estimatedDistanceToGoal(neighbor.to(),
+                                end));
+                }
+            }
+        }
+
+        if (timer.isTimeUp()) {
+            return new ShortestPathResult.Timeout<>(states, timer.elapsedDuration());
+        }
+
+        if (!distances.containsKey(end)) {
+            return new ShortestPathResult.Unsolvable<>(states, timer.elapsedDuration());
+        }
+
+        VERTEX current = end;
+        while (!current.equals(start)) {
+            solution.add(0, current);
+            current = prevPoint.get(current);
+        }
+        solution.add(0, start);
+        return new ShortestPathResult.Solved<>(
+            solution,
+            distances.get(end),
+            states,
+            timer.elapsedDuration()
+        );
     }
 
     @Override
     protected AStarGraph<VERTEX> graph() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return this.graph;
     }
 }
